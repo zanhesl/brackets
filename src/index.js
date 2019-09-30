ea6b408c1613baf7fffce4e059677f74ec4adb1e
Alexander Lavrukhine <zanhesl@gmail.com>
2019-09-30 20:20:29 +0300

    refactor: get rid of unnecessary ifs

--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
 module.exports =
-function check(str, bracketsConfig) {
-    const bracketsStack = [];                                                            // and Getting rid of undefined for further counting
+    function check(str, bracketsConfig) {
+        const bracketsStack = []; // create Stack structure
 
-    str = str.split(``);
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            if (bracketsConfig[j].includes(str[i])) {
-                if (bracketsConfig[j].indexOf(str[i]) === 0){
-                    if ((bracketsConfig[j][0] == bracketsConfig[j][1])&&(bracketsStack.length > 0)&&(bracketsStack[bracketsStack.length-1][0] == j)){
-                        bracketsStack.pop()
-                    } else {
-                        bracketsStack.push([j, `open`]);
-                    }
-                } else {
-                    if (((bracketsStack.length > 0)&&(bracketsStack[bracketsStack.length-1][0] == j)&&bracketsStack[bracketsStack.length-1][1] == `open`)) {
-                        bracketsStack.pop()
-                    } else {
-                        bracketsStack.push([j, `close`]);
+        str = str.split(``);
+        for (let i = 0; i < str.length; i++) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (bracketsConfig[j].includes(str[i])) { //stack operating 
+                    bracketsStack.push(str[i]);
+                    if ((bracketsStack.length >= 2) && (bracketsStack[bracketsStack.length - 1] == bracketsConfig[j][1]) &&
+                        (bracketsStack[bracketsStack.length - 2] == bracketsConfig[j][0])) {
+                        bracketsStack.pop();
+                        bracketsStack.pop();
                     }
                 }
             }
         }
-    }
-    if (bracketsStack.length === 0){
-        return true;
-    }
+        if (bracketsStack.length === 0) {
+            return true;
+        }
 
-    return false;
-}
+        return false;
+    }
